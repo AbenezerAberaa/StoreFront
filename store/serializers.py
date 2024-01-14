d0d95e9d43ca58f7fb2fa04a28df01df70375ddf
AbenezerAberaa <abenezer.aberaa@gmail.com>
2024-01-15 01:43:19 +0300

    update and delete cart items from shoping cart

--- store/serializers.py
+++ store/serializers.py
@@ -36,17 +36,62 @@ class SimpleProductSerializers(serializers.ModelSerializer):
     
 class CartItemSerializers(serializers.ModelSerializer):
     product = SimpleProductSerializers()
+    total_price = serializers.SerializerMethodField()
+    def get_total_price(self, cart_item):
+        return cart_item.product.unit_price * cart_item.quantity
+
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'quantity']
+        fields = ['id', 'product', 'quantity', 'total_price']
+
 
 class CartSerializers(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
-    items = CartItemSerializers(many=True)
+    items = CartItemSerializers(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+    def get_total_price(self, cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
 
     class Meta:
         model = Cart
-        fields = ['id', 'items']
+        fields = ['id', 'items', 'total_price']
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("No product is the given id exist")
+        return value
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+        
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity += quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+
+        return self.instance
+            
+
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product_id', 'quantity']
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']  
+
+
+    
 
 
     
