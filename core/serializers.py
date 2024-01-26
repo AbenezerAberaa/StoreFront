aa5fe194fdb1d401ebc6f087c3d206c0118a5df1
AbenezerAberaa <abenezer.aberaa@gmail.com>
2024-01-26 15:53:09 +0300

    Order cart API

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,14 @@
-from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserRegistrationSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserRegistrationSerializer, UserCreateSerializer as BaseUserCreateSerializer
+
+from StoreFront.store.models import Customer
 
 class UserRegistrationSerializer(BaseUserRegistrationSerializer):
     class Meta(BaseUserRegistrationSerializer.Meta):
+        fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name')
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+
+    class Meta(BaseUserCreateSerializer.Meta):
         fields = ('id', 'email', 'username', 'password', 'first_name', 'last_name')
 
 class UserSerializer(BaseUserSerializer):
